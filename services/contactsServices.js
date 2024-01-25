3e2149308dab8bf9b9e6fb9d2adefc07e99d9647
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2024-01-25 23:21:54 +0200

    hw02(done)

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,8 +1,13 @@
 import { readFile, writeFile } from "fs/promises";
 import path from "path";
 import { v4 as uuid } from "uuid";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const contactsPath = path.join(__dirname, "db", "contacts.json");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "../db", "contacts.json");
 
 export async function listContacts() {
   const list = await readFile(contactsPath);
